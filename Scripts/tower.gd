71856f8ca8037c6a09d9ef12a279e50aca4b2a8d
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2026-07-22 19:36:58 -0400

    Added base class tower and children towers

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -4,6 +4,14 @@ class_name Tower
 var health: int
 var range: int
 var deadzone: int
+var damage: int
+var firerate: float
 
-func inRange(pos: Vector2):
-	return
+func inRange(pos: Vector2) -> bool:
+	var dist = position.distance_to(pos)
+	return dist < range and dist > deadzone
+	
+func receiveDamage(damageTaken: int) -> void:
+	health -= damageTaken
+	if health <= 0:
+		queue_free()
