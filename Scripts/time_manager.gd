72617231af8fe2c427a8aa8c6cab6448b7bfb4e1
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2026-07-23 14:14:08 -0400

    Created time manager

---
+++ Scripts/time_manager.gd
@@ -0,0 +1,21 @@
+@tool
+extends Node
+class_name TimeManager
+
+@export var startTime: int = 100
+@export var paused: bool = false
+var timeElapsed: float = 0
+var timeModifications: int = 0
+
+func _physics_process(delta: float):
+	if paused:
+		return
+	timeElapsed += delta
+	var timeLeft: int = floor(startTime-timeElapsed+timeModifications)
+	print("Time left: ", timeLeft)
+
+func addTime(amount: int):
+	timeModifications += amount
+
+func spendTime(amount: int):
+	timeModifications -= amount
