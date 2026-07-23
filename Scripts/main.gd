553fc11d51b74822a6cd03cb7b8a67843536d81b
Shawn Halliday <114366487+ShawnHalliday@users.noreply.github.com>
2026-07-22 21:51:11 -0400

    Added tilemap layer

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 @export var building: bool = false
+@onready var ground: TileMapLayer = $Ground
 
-func _process(delta: float):
-	var towers = $Towers.get_children()
+#func _process(delta: float):
+	#var towers = $Towers.get_children()
 	
 	
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
-			print("Left mouse button released")
+			var tile = ground.local_to_map(get_global_mouse_position())
+			var tilepos = ground.map_to_local(tile)
+			print(tile, tilepos)
